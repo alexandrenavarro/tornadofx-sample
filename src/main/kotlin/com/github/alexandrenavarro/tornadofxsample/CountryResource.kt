2334a5232b353fc18dd5cb4b31cb2319af83ea84
Alexandre Navarro <alexandre.j.navarro@gmail.com>
2018-05-05 01:18:55 +0200

    Add Feign Resource

---
+++ src/main/kotlin/com/github/alexandrenavarro/tornadofxsample/CountryResource.kt
@@ -0,0 +1,11 @@
+package com.github.alexandrenavarro.tornadofxsample
+
+import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.RequestMethod
+
+
+interface CountryResource {
+
+    @RequestMapping(method = [(RequestMethod.GET)], value = "/rest/v2/all")
+    fun getCountries(): List<Country>
+}
